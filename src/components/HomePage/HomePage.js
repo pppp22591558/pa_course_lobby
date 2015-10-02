3139b2740cd6f0b1759d53f12a1573a06efe8d91
NeilZheng <pppp29654213@gmail.com>
2015-10-02 14:37:00 +0800

    for roger

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,14 +1,21 @@
-import React, { PropTypes, Component } from 'react';
-// import styles from './HomePage.css';
+import React, {Component} from 'react';
+import styles from './HomePage.css';
 import withStyles from '../../decorators/withStyles';
+import SubHeader from './SubHeader';
+import AppStore from '../../stores/app-store';
 
-// @withContext
-// @withStyles(styles)
+@withStyles(styles)
+
+
 class HomePage extends Component {
+  handleClick(){
+    AppStore.addItem('{id:1, name: "item1"}');
+  }
   render(){
     return(
-      <div className="homepage-wrapper">
-        <h1>This is homepage wrapper</h1>
+      <div>
+        <h1 className="HomePage-h1" onClick={this.handleClick}>HomePage fewf</h1>
+        <SubHeader/>
       </div>
     )
   }
---
+++ src/components/HomePage/SubHeader.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class SubHeader extends Component{
+  render(){
+    return(
+      <h2>SubHeader</h2>
+    )
+  }
+}
+
+export default SubHeader;
