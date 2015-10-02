b7755aef9697bce97ec821aea363a0a871255c84
NeilZheng <pppp29654213@gmail.com>
2015-10-02 17:23:33 +0800

    header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,25 +8,69 @@ import Navigation from '../Navigation';
 
 @withStyles(styles)
 class Header extends Component {
-
   render() {
     return (
-      <div className="Header">
-        <div className="Header-container">
-          <a className="Header-brand" href="/" onClick={Link.handleClick}>
-            <img className="Header-brandImg" src={require('./logo-small.png')} width="38" height="38" alt="React" />
-            <span className="Header-brandTxt">Your Company</span>
-          </a>
-          <Navigation className="Header-nav" />
-          <div className="Header-banner">
-            <h1 className="Header-bannerTitle">React</h1>
-            <p className="Header-bannerDesc">Complex web apps made easy</p>
+      <div className="main-nav">
+        <div className="nav-content">
+          <RoleOptions/>
+          <div className="nav-logo">
+            <a href="/"><img src={require('../../public/images/courses_lobby_logo.png')} alt="PaGamO logo"/></a>
           </div>
+          <UserPanel/>
+          <HiddenSideBar/>
         </div>
       </div>
-    );
+    )
   }
+}
 
+class RoleOptions extends Component {
+  state = {
+    activeRole: 'Student'
+  }
+  componentDidMount(){
+    console.log(this.state.activeRole);
+  }
+  render() {
+    return(
+      <ul className="role-options">
+        <li><a href="#"><img onClick={this.handleClick} src={require('../../public/images/role1.png')}/></a></li>
+        <li><a href="#"><img onClick={this.handleClick} src={require('../../public/images/role2.png')}/></a></li>
+        <li><h5><span className="role">Role:</span> {this.state.activeRole}</h5></li>
+      </ul>
+    )
+  }
+}
+
+class UserPanel extends Component {
+  render(){
+    return(
+      <ul className="user-panel">
+        <li><a href="#"><img src={require('../../public/images/list.svg')}/></a></li>
+        <li className="purchase-cart">
+          <a href="#"><img src={require('../../public/images/cart.svg')}/></a>
+          <div className="cart-popup"></div>
+        </li>
+        <li><h5><a href="#">Hi, Neil</a></h5></li>
+      </ul>
+    )
+  }
+}
+
+class HiddenSideBar extends Component {
+  render(){
+    return(
+      <div style={{display:'none'}} className="hidden-sidebar">
+        <ul>
+          <li>list item 1</li>
+          <li>list item 2</li>
+          <li>list item 3</li>
+          <li>list item 4</li>
+          <li>list item 5</li>
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default Header;
